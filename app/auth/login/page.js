9d9ec2eda4a31c06e8ea08ecdc42e477b4f46b23
SentryoZ <dat03021999@gmail.com>
2024-05-25 12:31:05 +0700

    base login

---
+++ app/auth/login/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from '@/app/page.module.css';
+
+export default function Login() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.description}>
+               Login page
+            </div>
+        </main>
+    );
+}
