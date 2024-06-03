603b6411fb1fb933f82fa1150d297a41f8710832
tAmh29 <ttrhuynh07@gmail.com>
2024-06-03 00:43:24 -0700

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "/", // forward all paths to the root
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
