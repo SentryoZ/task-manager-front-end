9c117b4dbde12050751c37f71b5ecb91a9ee919e
SentryoZ <dat03021999@gmail.com>
2024-06-04 19:44:46 +0700

    update auth

--- components/auth/loginform.tsx
+++ components/auth/loginform.tsx
@@ -2,104 +2,107 @@
 
 import React from "react";
 import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardContent,
+    CardFooter,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { sendUnauthenticatedRequest } from "@/lib/http";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {axiosInstance, sendUnauthenticatedRequest} from "@/lib/http";
 import Link from "next/link";
-import { fetchCsrfToken } from "@/lib/http";
+import {useRouter} from "next/navigation";
 
 const LoginForm = () => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const router = useRouter()
 
-  const onSubmit = async (e) => {
-    e.preventDefault(); // Prevent the default form submission behavior
+    const onSubmit = async (e) => {
+        // Prevent the default form submission behavior
+        e.preventDefault();
 
-    const data = [email, password]; // Prepare the data to be sent
-    console.log("Submitting login form with data:", data); // Log the data being sent
+        // Prepare the data to be sent
+        const data = {email, password};
+        try {
+            axiosInstance.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then(response => {
+                sendUnauthenticatedRequest(
+                    "POST",
+                    "http://127.0.0.1:8000/api/auth/login",
+                    data
+                ).then(function (result) {
+                    console.log(result)
+                    // Save to local storage and redirect to home page
+                    localStorage.setItem("access_token", result.token)
+                    router.push('/')
+                });
+            });
+        } catch (error) {
+            console.error("Login failed:", error); // Log any errors
+        }
+    };
 
-    try {
-      // Send the login request
-      const result = await sendUnauthenticatedRequest(
-        "POST",
-        "http://127.0.0.1:8000/api/auth/login",
-        data
-      );
-      console.log("Login successful:", result); // Log the successful response
-
-      // Save to local storage
-      localStorage.setItem("access_token", result.token);
-    } catch (error) {
-      console.error("Login failed:", error); // Log any errors
-    }
-  };
-
-  return (
-    <Card className="grid items-center w-full max-w-[400px] h-full max-h-[372px] border-none shadow-none">
-      <CardHeader>
-        <div className="flex items-center justify-center">
-          <Avatar style={{ width: "100px", height: "100px" }}>
-            <AvatarImage
-              src="https://static-00.iconduck.com/assets.00/profile-circle-icon-2048x2048-cqe5466q.png"
-              alt="N/A"
-            />
-          </Avatar>
-        </div>
-        <CardTitle className="text-1xl font-bold text-center">
-          Company Name
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <form className="space-y-2">
-          <div className="space-x-2">
-            <Input
-              id="email"
-              name="email"
-              type="text"
-              placeholder="email@domain.com"
-              value={email} // Bind value to state
-              onChange={(e) => {
-                setEmail(e.target.value); // Update state on change
-              }}
-            />
-          </div>
-          <div className="space-y-2">
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              placeholder="password"
-              value={password} // Bind value to state
-              onChange={(e) => {
-                setPassword(e.target.value); // Update state on change
-              }}
-            />
-          </div>
-          <div className="flex justify-center">
-            <Button type="submit" onClick={onSubmit} className="w-full">
-              Login
-            </Button>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex flex-col space-y-1.438">
-        <Link
-          href="/auth/resetpassword"
-          className="text-sm text-blue-500 hover:text-blue-700 text-center"
-        >
-          Forgot password?
-        </Link>
-      </CardFooter>
-    </Card>
-  );
+    return (
+        <Card className="grid items-center w-full max-w-[400px] h-full max-h-[372px] border-none shadow-none">
+            <CardHeader>
+                <div className="flex items-center justify-center">
+                    <Avatar style={{width: "100px", height: "100px"}}>
+                        <AvatarImage
+                            src="https://static-00.iconduck.com/assets.00/profile-circle-icon-2048x2048-cqe5466q.png"
+                            alt="N/A"
+                        />
+                    </Avatar>
+                </div>
+                <CardTitle className="text-1xl font-bold text-center">
+                    Company Name
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <form className="space-y-2">
+                    <div className="space-x-2">
+                        <Input
+                            id="email"
+                            name="email"
+                            type="text"
+                            placeholder="email@domain.com"
+                            value={email} // Bind value to state
+                            onChange={(e) => {
+                                setEmail(e.target.value); // Update state on change
+                            }}
+                        />
+                    </div>
+                    <div className="space-y-2">
+                        <Input
+                            id="password"
+                            name="password"
+                            type="password"
+                            placeholder="password"
+                            value={password} // Bind value to state
+                            onChange={(e) => {
+                                setPassword(e.target.value); // Update state on change
+                            }}
+                        />
+                    </div>
+                    <div className="flex justify-center">
+                        <Button type="submit" onClick={onSubmit} className="w-full">
+                            Login
+                        </Button>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex flex-col space-y-1.438">
+                <Link
+                    href="/auth/resetpassword"
+                    className="text-sm text-blue-500 hover:text-blue-700 text-center"
+                >
+                    Forgot password?
+                </Link>
+            </CardFooter>
+        </Card>
+    );
 };
 
 export default LoginForm;
